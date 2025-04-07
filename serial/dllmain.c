4fff852bc51c2fb13b5e8562744130d7a5d65a47
ElMati555 <122708175+lvplay2@users.noreply.github.com>
2025-04-07 16:32:31 -0300

    stuff

---
+++ serial/dllmain.c
@@ -0,0 +1,25 @@
+/*==========================================================================
+ *
+ *  Copyright (C) 1995 Microsoft Corporation.  All Rights Reserved.
+ *
+ *  File:       dllmain.c
+ *  Content:	DPLAY.DLL initialization
+ *  History:
+ *   Date	By	Reason
+ *   ====	==	======
+ *
+ ***************************************************************************/
+
+#include "windows.h"
+
+/*
+ * DllMain
+ */
+HINSTANCE hInst;
+
+BOOL WINAPI DllMain(HINSTANCE hmod, DWORD dwReason, LPVOID lpvReserved)
+{
+    hInst = hmod;
+
+    return TRUE;
+} /* DllMain */
